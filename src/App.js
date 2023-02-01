d8f1f7fb413cf04453cced2ee29641522def48d7
Saif Ul islam <91761499+syousafzai861@users.noreply.github.com>
2023-02-01 12:33:58 +0500

    add and subtract value
    in this i have changed the state by passing the payloads from the store and perform add and subtract function task

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import { useSelector,useDispatch } from 'react-redux';
 import './App.css';
 
 function App() {
+let dispatch = useDispatch();
+let state = useSelector(state=>state);
+console.log(state)
   return (
+
     <div className="App">
       <h1>Hello Redux</h1>
+    {state.name}
+    {state.value}
+    
+    <button onClick={()=>{dispatch({type: "ADD", payload: 2} )}}>ADD</button>
+    <button onClick={()=>{dispatch({type: "SUB", payload: 2} )}}>Subtract</button>
     </div>
   );
 }
--- src/Store.js
+++ src/Store.js
@@ -1,9 +1,14 @@
 import { createStore } from "redux";
 
-let CounterReducer = (state, action) => {
+let CounterReducer = (state = { value: 100, name: "saif redux" }, action) => {
+  if (action.type === "ADD") {
+    return { ...state, value: state.value + action.payload };
+  }
+  if (action.type === "SUB") {
+    return { ...state, value: state.value - action.payload };
+  }
   return state;
 };
-
 let store = createStore(CounterReducer);
 
 export default store;
